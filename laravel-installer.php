668c3830436138f558f69f8bd18770e6cc28dd2c
Dali Hillary <dalirichardh@gmail.com>
2023-08-04 09:11:24 +0300

    manual composer

--- laravel-installer.php
+++ laravel-installer.php
@@ -1,7 +1,7 @@
 <?php
 // Use ls command to shell_exec
 // function
-$output = shell_exec('ls');
+$output = shell_exec('php composer.phar install');
   
 // Display the list of all file
 // and directory
